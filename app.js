2e2cd9ab2b812b51b53e654c3636f4014f05f5f4
adevadiga <abhinandhan.d@gmail.com>
2020-06-26 09:12:10 +0200

    Lit HTML basi example

---
+++ app.js
@@ -0,0 +1,17 @@
+ import {html, render} from './node_modules/lit-html/lit-html.js';
+
+const myTemplate = (name) => 
+html   `
+<div>
+<h1 id="hello">Hello ${name}</h1>
+
+</div>
+`
+
+render(myTemplate('Abhinandhan'), document.body)
+
+//comparing innerHTML update & lit-HTML update
+setTimeout(()=>{
+    document.getElementById('hello').innerHTML = "Hello Nandhan Devadiga"
+   // render(myTemplate('Nandhan Devadiga'), document.body)  
+},5000)
